b38343d94c3a09e4bbe7b6c3f7bfce5986d707ba
Petr Vávro <BlackSun.Petr@gmail.com>
2014-05-14 16:17:11 +0200

    Aspose via pdfextractor - better api, result almost the same

--- FullTextIndexTests/Program.cs
+++ FullTextIndexTests/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Aspose.Pdf.Facades;
 using Aspose.Pdf.Text;
 using Aspose.Pdf.Text.TextOptions;
 
@@ -26,11 +27,22 @@ namespace FullTextIndexTests
             var extension = Path.GetExtension(fileName);
             if (extension == ".pdf")
             {
-                var doc = new Aspose.Pdf.Document(fileName);
-                var textAbsorber = new TextAbsorber();
-                doc.Pages.Accept(textAbsorber);
+                PdfExtractor pdfExtractor = new PdfExtractor();
+                pdfExtractor.BindPdf(fileName);
 
-                var text = textAbsorber.Text;
+                //use parameterless ExtractText method
+                pdfExtractor.ExtractText();
+
+                MemoryStream tempMemoryStream = new MemoryStream();
+                pdfExtractor.GetText(tempMemoryStream);
+
+                string text = "";
+                //specify Unicode encoding type in StreamReader constructor
+                using (StreamReader streamReader = new StreamReader(tempMemoryStream, Encoding.Unicode))
+                {
+                    streamReader.BaseStream.Seek(0, SeekOrigin.Begin);
+                    text = streamReader.ReadToEnd();
+                }
 
                 Console.WriteLine(text);
             }
