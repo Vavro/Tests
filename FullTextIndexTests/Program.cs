bffe59a8801b1fa8910de9470920114c115fdcd5
Petr Vávro <BlackSun.Petr@gmail.com>
2014-05-14 15:44:13 +0200

    basic open file dialog

--- FullTextIndexTests/Program.cs
+++ FullTextIndexTests/Program.cs
@@ -1,15 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace FullTextIndexTests
 {
     class Program
     {
-        static void Main(string[] args)
+        [STAThread]
+        private static void Main(string[] args)
         {
+            var dialog = new OpenFileDialog()
+                         {
+                             Multiselect = false
+                         };
+            dialog.ShowDialog();
+
+            var fileName = dialog.FileName;
+
+            var extension = Path.GetExtension(fileName);
+            if (extension == ".pdf")
+            {
+
+            }
         }
     }
 }
