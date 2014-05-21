3b0cc47ebc1dcb70e78d0a45de36d1377ebac366
Petr Vávro <BlackSun.Petr@gmail.com>
2014-05-21 11:34:57 +0200

    first interface scetch

---
+++ FullTextIndexTests/Fulltext/ZaznamFullTextIndexer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FullTextIndexTests.Fulltext
+{
+    public class ZaznamFulltextIndexer
+    {
+        public void AddZaznamToFulltext(ZaznamFulltextDocument insertedDocument)
+        {
+            
+        }
+
+        public void UpdateZaznamMetadata(string id, ZaznamMetadata metadata)
+        {
+            
+        }
+
+        public void UpdateZaznamInFulltext(ZaznamFulltextDocument updatedDocument)
+        {
+            
+        }
+    }
+
+    public class ZaznamFullTextSearch
+    {
+        //todo: return type
+        //todo: extending more metadata
+        public void SearchIndex(DateTime? from, DateTime? to, string searchedText)
+        {
+            
+        }
+    }
+}
---
+++ FullTextIndexTests/Fulltext/ZaznamMetadata.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FullTextIndexTests.Fulltext
+{
+    public class ZaznamMetadata
+    {
+        public DateTime VytvoreniZaznamu { get; set; }
+        //todo: add PP, ORG, USR identifikaci
+    }
+
+    public class ZaznamFulltextDocument
+    {
+        public string Id { get; set; }
+        public ZaznamMetadata ZaznamMetadata { get; set; }
+        public FileFulltextInfo FileFulltextInfo { get; set; }
+    }
+
+    public class FileFulltextInfo
+    {
+        public string FileName { get; set; }
+        public Stream FileStream { get; set; }
+    }
+}
