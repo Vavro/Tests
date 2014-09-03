d12f3cbfcb0328f7e1e6780cd12250cc76fe7bb2
Petr Vavro <BlackSun.Petr@gmail.com>
2014-09-03 16:23:19 +0200

    Test fact with nunit

---
+++ DotNetRdfTests/DotNetRdfTests/EventTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests
+{
+    public delegate void TestDelegate(object sender, EventArgs e);
+
+    public class EventTest
+    {
+        public event TestDelegate TestEvent;
+
+        [Fact]
+        public void WillCallMultipleTimes()
+        {
+            TestEvent += OnTestEvent;
+            TestEvent += OnTestEvent;
+
+            TestEvent(this, new EventArgs());
+
+            TestEvent -= OnTestEvent;
+            Console.WriteLine("First removed!");
+            TestEvent(this, new EventArgs());
+
+            TestEvent -= OnTestEvent;
+            Console.WriteLine("Second removed!");
+            if (TestEvent != null)
+                TestEvent(this, new EventArgs());
+
+            TestEvent -= OnTestEvent;
+            Console.WriteLine("Third removed!");
+        }
+
+        private void OnTestEvent(object sender, EventArgs eventArgs)
+        {
+            Console.WriteLine("called!");
+        }
+    }
+}
