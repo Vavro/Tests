ca16ef7341d6f5b34e2516fd4522b5af960e2fd5
Petr Vavro <BlackSun.Petr@gmail.com>
2014-09-03 18:20:18 +0200

    try copying bower files to the testaspnetspa site

--- TestAspNetSpa/gulpfile.js
+++ TestAspNetSpa/gulpfile.js
@@ -1,5 +1,11 @@
 var gulp = require('gulp');
+var gulpBowerFiles = require('gulp-bower-files');
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task("bower-files", function () {
+    gulpBowerFiles().pipe(gulp.dest("./TestAspNetSpa/lib"));
+});
+
+gulp.task('default', ["bower-files"], function() {
+    // place code for your default task here
+    
 });
