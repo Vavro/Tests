511da3a4917868eb157f01a93a1c9262e6578cef
Petr Vávro <BlackSun.Petr@gmail.com>
2014-10-09 12:35:38 +0200

    Speedup? probably not according to msdn readers and writers now use own buffer

--- BigFileTransformation/BigFileTransformation/Program.cs
+++ BigFileTransformation/BigFileTransformation/Program.cs
@@ -12,13 +12,17 @@ namespace BigFileTransformation
     class Program
     {
         private const int FlushAfterLines = 100*1000;
+        private const string path = @"D:\TestTP\triada_export.txt";
+        private const string outputFile = @"D:\TestTP\output2.txt";
 
         static void Main(string[] args)
         {
             var timer = new Stopwatch();
             timer.Start();
-            using (var reader = System.IO.File.OpenText(@"D:\TestTP\triada_export.txt"))
-            using (var writer = new StreamWriter(@"D:\TestTP\output.txt", false))
+            using (FileStream fs = File.Open(path, FileMode.Open, FileAccess.Read, FileShare.None))
+            using (BufferedStream bs = new BufferedStream(fs))
+            using (StreamReader reader = new StreamReader(bs))
+            using (var writer = new StreamWriter(outputFile, false))
             {
                 string line = null;
                 int processedLines = 0;
@@ -41,7 +45,7 @@ namespace BigFileTransformation
                     if (processedLines % FlushAfterLines == 0)
                     {
                         processed10kLinesCount++;
-                        Console.WriteLine("Processed {0} lines. Elapsed time: {1:T}", processed10kLinesCount*FlushAfterLines, timer.Elapsed);
+                        Console.WriteLine("Processed {0:N} lines. Elapsed time: {1:T}", processed10kLinesCount*FlushAfterLines, timer.Elapsed);
                         writer.Flush();
                     }
                 }
