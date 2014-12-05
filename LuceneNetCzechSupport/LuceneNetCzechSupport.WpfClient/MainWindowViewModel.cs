0484a3e2fa498b8207e4f4252a65480a2ff46c47
Petr Vávro <BlackSun.Petr@gmail.com>
2014-12-05 11:18:41 +0100

    Add app for visual testing of analyzers, with basic layout

---
+++ LuceneNetCzechSupport/LuceneNetCzechSupport.WpfClient/MainWindowViewModel.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Lucene.Net.Analysis;
+using LuceneNetCzechSupport.Analyzers;
+using LuceneNetCzechSupport.WpfClient.Helpers;
+
+namespace LuceneNetCzechSupport.WpfClient
+{
+    public class MainWindowViewModel
+    {
+        public MainWindowViewModel()
+        {
+            var supportedAnalyzers = new Analyzer[] { 
+                SupportedAnalyzers.CzechAnalyzer, 
+                SupportedAnalyzers.CzechLightSnowballAnalyzer, 
+                SupportedAnalyzers.CzechAggresiveSnowballAnalyzer, 
+                SupportedAnalyzers.CzechHunspellAnalyzer };
+
+            AwailibleAnalyzers = supportedAnalyzers.Select(a => new AnalyzerViewModel(a)).ToList();
+
+            OpenFileCommand = new DelegateCommand
+        }
+
+        public string FileName
+        {
+            get;
+            set;
+        }
+
+        public ICommand OpenFileCommand
+        {
+            get;
+            set;
+        }
+
+        public ICommand OpenDirectoryCommand
+        {
+            get;
+            set;
+        }
+
+        public ICommand ClearIndexedDocumentsCommand
+        {
+            get;
+            set;
+        }
+
+        public string SearchText
+        {
+            get;
+            set;
+        }
+
+        public ICommand SearchCommand
+        {
+            get;
+            set;
+        }
+
+        public string SearchResult
+        {
+            get;
+            set;
+        }
+
+        public ICommand IndexSelectedDocumentsCommand { get; set; }
+
+        public AnalyzerViewModel SelectedAnalyzer { get; set; }
+
+        public List<AnalyzerViewModel> AwailibleAnalyzers { get; set; }
+    }
+
+    public class AnalyzerViewModel
+    {
+        public AnalyzerViewModel(Analyzer analyzer)
+        {
+            Analyzer = analyzer;
+        }
+
+        public Analyzer Analyzer { get; set; }
+        public string Name { get { return Analyzer != null ? Analyzer.ToString() : null; } }
+    }
+}
