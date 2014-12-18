a7519637f961d11dc38c313f4b622819805f0b19
Petr Vávro <BlackSun.Petr@gmail.com>
2014-12-18 12:22:23 +0100

    Test condition on related not included values

--- EntityFrameworkTests/EntityFrameworkTests/Program.cs
+++ EntityFrameworkTests/EntityFrameworkTests/Program.cs
@@ -11,6 +11,31 @@ namespace EntityFrameworkTests
     {
         static void Main(string[] args)
         {
+            //TestNotIncludedQuery();
+
+            using (var context = new NORTHWNDEntities())
+            {
+                context.Configuration.LazyLoadingEnabled = false;
+                context.Configuration.ProxyCreationEnabled = false;
+                context.Configuration.AutoDetectChangesEnabled = false;
+                context.Configuration.ValidateOnSaveEnabled = false;
+
+                var ordersQuery = context.Orders.Where(o => o.Customers.Region != o.Employees.Region);
+                var orders = ordersQuery.ToList();
+
+                foreach (var order in orders)
+                {
+                    PrintOrder(order);
+                }
+            }
+
+            Console.WriteLine("Press ENTER to exit");
+            Console.ReadLine();
+
+        }
+
+        private static void TestNotIncludedQuery()
+        {
             using (var context = new NORTHWNDEntities())
             {
                 context.Configuration.LazyLoadingEnabled = false;
@@ -26,13 +51,16 @@ namespace EntityFrameworkTests
 
                 foreach (var currentCustomerOrder in currentCustomerOrders)
                 {
-                    Console.WriteLine("Order Id {0}, shipped to: {4} {1} {2} {3}, on date {5}", currentCustomerOrder.OrderID, currentCustomerOrder.ShipAddress, currentCustomerOrder.ShipCity, currentCustomerOrder.ShipCountry, currentCustomerOrder.ShipName, currentCustomerOrder.ShippedDate);
+                    PrintOrder(currentCustomerOrder);
                 }
             }
+        }
 
-            Console.WriteLine("Press ENTER to exit");
-            Console.ReadLine();
-
+        private static void PrintOrder(Orders currentCustomerOrder)
+        {
+            Console.WriteLine("Order Id {0}, shipped to: {4} {1} {2} {3}, on date {5}", currentCustomerOrder.OrderID,
+                currentCustomerOrder.ShipAddress, currentCustomerOrder.ShipCity, currentCustomerOrder.ShipCountry,
+                currentCustomerOrder.ShipName, currentCustomerOrder.ShippedDate);
         }
     }
 }
